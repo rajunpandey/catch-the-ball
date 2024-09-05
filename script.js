979efebd05803951fe882bc3dde342b74efd0430
rajun pandey <73828661+rajunpandey@users.noreply.github.com>
2024-09-05 12:11:55 +0545

    Update script.js
    change speed and drag option

--- script.js
+++ script.js
@@ -16,6 +16,10 @@ let ballY;
 let ballDx;
 let ballDy;
 let score;
+let speedIncreaseFactor = 1.1; 
+let isDragging = false;
+let rightPressed = false;
+let leftPressed = false;
 
 function initGame() {
     paddleX = (canvas.width - paddleWidth) / 2;
@@ -24,17 +28,60 @@ function initGame() {
     ballDx = 2;
     ballDy = 2;
     score = 0;
+    isDragging = false;
+    rightPressed = false;
+    leftPressed = false;
     restartBtn.style.display = "none"; 
     gameOverMessage.classList.add('hidden'); 
     draw();
 }
 
-canvas.addEventListener("mousemove", (e) => {
-    const relativeX = e.clientX - canvas.offsetLeft;
-    if (relativeX > 0 && relativeX < canvas.width) {
-        paddleX = relativeX - paddleWidth / 2;
+
+canvas.addEventListener("touchstart", (e) => {
+    const relativeX = e.touches[0].clientX - canvas.offsetLeft;
+    if (relativeX > paddleX && relativeX < paddleX + paddleWidth) {
+        isDragging = true;
     }
 });
+
+canvas.addEventListener("touchmove", (e) => {
+    if (isDragging) {
+        const relativeX = e.touches[0].clientX - canvas.offsetLeft;
+        if (relativeX > 0 && relativeX < canvas.width) {
+            paddleX = relativeX - paddleWidth / 2;
+        }
+    }
+});
+
+canvas.addEventListener("touchend", () => {
+    isDragging = false;
+});
+
+// Mouse drag control for desktop
+canvas.addEventListener("mousedown", (e) => {
+    const relativeX = e.clientX - canvas.offsetLeft;
+    if (relativeX > paddleX && relativeX < paddleX + paddleWidth) {
+        isDragging = true;
+    }
+});
+
+canvas.addEventListener("mousemove", (e) => {
+    if (isDragging) {
+        const relativeX = e.clientX - canvas.offsetLeft;
+        if (relativeX > 0 && relativeX < canvas.width) {
+            paddleX = relativeX - paddleWidth / 2;
+        }
+    }
+});
+
+canvas.addEventListener("mouseup", () => {
+    isDragging = false;
+});
+
+canvas.addEventListener("mouseleave", () => {
+    isDragging = false;
+});
+
 
 function drawBall() {
     ctx.beginPath();
@@ -72,18 +119,25 @@ function moveBall() {
         if (ballX > paddleX && ballX < paddleX + paddleWidth) {
             ballDy = -ballDy;
             score++;
+
+            // Increase ball speed after each score
+            ballDx *= speedIncreaseFactor;
+            ballDy *= speedIncreaseFactor;
         } else {
             gameOver();
         }
     }
 }
 
+
 function gameOver() {
     finalScoreElement.textContent = score; 
     gameOverMessage.classList.remove('hidden');
-    alert("GAME OVER! Final Score: " + score);
+    gameOverMessage.classList.add('show');
+    // alert("GAME OVER! Final Score: " + score);
     restartBtn.style.display = "block";
 }
+
 
 function draw() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
@@ -98,6 +152,8 @@ function draw() {
 }
 
 function restartGame() {
+    gameOverMessage.classList.remove('show');
+    gameOverMessage.classList.add('hidden');
     initGame();
 }
 
